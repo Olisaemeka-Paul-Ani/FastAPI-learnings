604a6a00f44e489a39bf4a8d68ab55c77e4c9c3a
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-07-16 10:45:28 -0700

    Implement
    post method

--- myscript.py
+++ myscript.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,6 +12,11 @@ students = {
   }
 }
 
+
+class Student(BaseModel):
+  name : str
+  age: int
+  year: str
 
 @app.get("/")
 def index(): 
@@ -27,3 +33,14 @@ def get_student(*,student_id:int, name: Optional[str] =None , test : int ):
     if students[student_id]["name"] == name:
       return students[student_id]
   return {"Data":" Not found"}
+
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id:  int, student: Student):
+  if student_id in students:
+    return {"Error": "Student exists"}
+  
+  students[student_id]  = student
+  return students[student_id ]
+
+
